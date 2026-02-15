e1c7dc9dde29bee027d0d9e5431b634842509a3c
danish ahmed <dan.ahmed@live.com>
2026-02-15 17:04:31 +0500

    mod aws workflow

--- aws/provider.tf
+++ aws/provider.tf
@@ -1,3 +1,16 @@
+terraform {
+  backend "s3" {
+    bucket         = "tfstate-euw2-01"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-2"
+    dynamodb_table = "terraform-locks"
+    encrypt        = true
+  }
+}
+
+
+
+
 provider "aws" {
   region = var.aws_region
 }
